47f8a9c54a0cb86068598e71d1c54e21f7b972d7
Richi Garza <richi.garzar@gmail.com>
2017-04-10 01:51:29 -0500

    Se preparan para se llamado por JS
    Se agrega el FORM, se agregan los id a los campos, se agrega el name
    para que sea validado.

--- Forms/Captura.php
+++ Forms/Captura.php
@@ -3,88 +3,90 @@
           Panel de Captura
         </div>
         <div class="panel panel-body">
-          <div class="row">
-            <div class="col-md-4">
-              <label>Folio</label>
-              <input id="txtFolio2" type="text" class="form-control txtFolio" placeholder="Folio" disabled>
+          <form name="formCaptura">
+            <div class="row">
+              <div class="col-md-4">
+                <label>Folio</label>
+                <input id="txtFolio2" type="text" class="form-control txtFolio" placeholder="Folio" disabled>
+              </div>
+              <div class="col-md-4">
+                <label>Fecha</label>	
+                <input id="txtFecha2" type="text" class="form-control txtFecha" placeholder="Fecha" disabled>
+              </div>
+              <div class="col-md-4">
+                <label>Hora</label>
+                <input id="txtHora2" type="text" class="form-control txtHora" placeholder="Hora" disabled>
+              </div>                        
             </div>
-            <div class="col-md-4">
-              <label>Fecha</label>	
-              <input id="txtFecha2" type="text" class="form-control txtFecha" placeholder="Fecha" disabled>
+            <div class="row">
+              <div class="col-md-4">
+                <label>Ocupación</label>
+                <select class="form-control" placeholder="Ocupación" id="ddlOcupacion" name="ddlOcupacion">
+                  <option selected value="-1" disabled>Seleccione ocupación</option>
+                  <option value="1">Desempleado</option>
+                  <option value="2">Empleado Sector Público</option>
+                  <option value="3">Empleado Sector Privado</option>
+                  <option value="4">Dueño de Empresa</option>
+                  <option value="5">Profesionista Independiente</option>
+                  <option value="6">Jubilado</option>
+                  <option value="7">Estudiante</option>
+                </select>           
+              </div>
+              <div class="col-md-4">
+                <label>Escolaridad</label>
+                <select class="form-control" placeholder="Escolaridad" id="ddlEscolaridad" name="ddlEscolaridad">
+                  <option selected value="-1" disabled>Seleccione último grado de escolaridad</option>
+                  <option value="1">Ninguno</option>
+                  <option value="2">Primaria</option>
+                  <option value="3">Secundaria</option>
+                  <option value="4">Preparatoria</option>
+                  <option value="5">Carrera Técnica</option>
+                  <option value="6">Licenciatura</option>
+                  <option value="7">Maestría / Doctorado</option>
+                </select>
+              </div>  
+              <div class="col-md-4">
+                <label>Estandar buscado</label>
+                <select id="ddlEstandar" name="ddlEstandar" class="form-control select2" placeholder="Estandar Buscado">
+                  <option selected value="-1" disabled>Seleccione estandar buscado</option>                
+                </select>
+              </div>                      
             </div>
-            <div class="col-md-4">
-              <label>Hora</label>
-              <input id="txtHora2" type="text" class="form-control txtHora" placeholder="Hora" disabled>
-            </div>                        
-          </div>
-          <div class="row">
-            <div class="col-md-4">
-              <label>Ocupación</label>
-              <select class="form-control" placeholder="Ocupación" id="ddlOcupacion">
-                <option selected value="-1" disabled>Seleccione ocupación</option>
-                <option value="1">Desempleado</option>
-                <option value="2">Empleado Sector Público</option>
-                <option value="3">Empleado Sector Privado</option>
-                <option value="4">Dueño de Empresa</option>
-                <option value="5">Profesionista Independiente</option>
-                <option value="6">Jubilado</option>
-                <option value="7">Estudiante</option>
-              </select>           
-            </div>
-            <div class="col-md-4">
-              <label>Escolaridad</label>
-              <select class="form-control" placeholder="Escolaridad" id="ddlEscolaridad">
-                <option selected value="-1" disabled>Seleccione último grado de escolaridad</option>
-                <option value="1">Ninguno</option>
-                <option value="2">Primaria</option>
-                <option value="3">Secundaria</option>
-                <option value="4">Preparatoria</option>
-                <option value="5">Carrera Técnica</option>
-                <option value="6">Licenciatura</option>
-                <option value="7">Maestría / Doctorado</option>
-              </select>
-            </div>  
-            <div class="col-md-4">
-              <label>Estandar buscado</label>
-              <select id="ddlEstandar" class="form-control select2" placeholder="Estandar Buscado">
-                <option selected value="-1" disabled>Seleccione estandar buscado</option>                
-              </select>
-            </div>                      
-          </div>
-          <div class="row">
-            <div class="col-md-4">
-              <label>Estado donde se localiza</label>
-              <select id="ddlEstadoEstandar" class="form-control select2" placeholder="Estado donde se localiza">
-                <option selected value="-1" disabled>Seleccione estado donde se localiza</option>
-              </select>             
-            </div>
-            <div class="col-md-4">
-              <label>Prestador del servicio</label>
-              <select id="ddlPrestadorEstadoEstandar" class="form-control select2" placeholder="Estado donde se localiza">
-                <option selected value="-1" disabled>Seleccione prestador de serivicio</option>
-              </select>             
-            </div>
-            <div class="col-md-4">
-            	<label>Representante</label>
-              <div class="input-group input-group-sm ">
-                <div class="select2-container">
-                  <select id="ddlRepresentante" class="form-control select2" placeholder="Prestador de Servicio">
-                    <option selected value="-1" disabled>Seleccione al representante</option>
-                  </select>
-                </div>
-                <div class="input-group-btn">
-                  <button id="btnVerRepresentante" class="btn btn-info">
-                    <span class="glyphicon glyphicon-search"></span> ver
-                  </button>
+            <div class="row">
+              <div class="col-md-4">
+                <label>Estado donde se localiza</label>
+                <select id="ddlEstadoEstandar" name="ddlEstadoEstandar" class="form-control select2" placeholder="Estado donde se localiza">
+                  <option selected value="-1" disabled>Seleccione estado donde se localiza</option>
+                </select>             
+              </div>
+              <div class="col-md-4">
+                <label>Prestador del servicio</label>
+                <select id="ddlPrestadorEstadoEstandar" name="ddlPrestadorEstadoEstandar" class="form-control select2" placeholder="Estado donde se localiza">
+                  <option selected value="-1" disabled>Seleccione prestador de serivicio</option>
+                </select>             
+              </div>
+              <div class="col-md-4">
+              	<label>Representante</label>
+                <div class="input-group input-group-sm ">
+                  <div class="select2-container">
+                    <select id="ddlRepresentante" name="ddlRepresentante" class="form-control select2" placeholder="Prestador de Servicio">
+                      <option selected value="-1" disabled>Seleccione al representante</option>
+                    </select>
+                  </div>
+                  <div class="input-group-btn">
+                    <button id="btnVerRepresentante" class="btn btn-info">
+                      <span class="glyphicon glyphicon-search"></span> ver
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
 
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <button id="btnCapturar" class="btn btn-success"><i class="glyphicon glyphicon-plus"></i> Capturar</button>
             </div>
-          </div>        
+            <div class="row">
+              <div class="col-md-12">
+                <button id="btnCapturar" type="submit" class="btn btn-success"><i class="glyphicon glyphicon-plus"></i> Capturar</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
--- Forms/Monitor.php
+++ Forms/Monitor.php
@@ -3,7 +3,7 @@
           Panel de Atención
         </div>
         <div class="panel panel-body">
-          <form id="formRegistrar" name="formRegistrar" action="">
+          <form id="formMonitor" name="formMonitor" action="">
             <div class="row">
               <div class="col-md-4">
                 <label>Folio</label>
@@ -21,39 +21,36 @@
             <div class="row panel-margin">
               <div class="col-md-3">
                 <label>Motivo</label>
-                <select id="Motivo" class="form-control">
-                  <option>Informativo</option>
-                  <option>Trámite</option>
-                  <option>Requisitos</option>
-                  <option>Seguimiento</option>                        
+                <select id="ddlMotivo" name="ddlMotivo" class="form-control">
+                  <option value="1">Informativo</option>
+                  <option value="2">Trámite</option>
+                  <option value="3">Requisitos</option>
+                  <option value="4">Seguimiento</option>                        
                 </select>
               </div>
             </div>
             <div class="row panel-margin">
               <div class="col-md-4">
                 <label>Asunto</label>
-                <textarea id="Asunto" class="form-control" rows="3" placeholder="Asunto"></textarea>
+                <textarea id="txtAreaAsunto" name="txtAreaAsunto" class="form-control" rows="3" placeholder="Asunto"></textarea>
               </div>
             </div>
             <div class="row panel-margin">
               <div class="col-md-6">
                 <label>Dirigido a</label>
-                <select class="form-control">
+                <select name="ddlDirigidoA" id="ddlDirigidoA" class="form-control">
                   <option selected value="-1" disabled>Seleccione a donde va dirgido</option>
-                  <option>DIRECCIÓN DE PROMOCÍON Y APOYO A PRESTADORES (ACREDITACIÓN INICIAL)</option>
-                  <option>DIRECCIÓN DE ACREDITACIÓN Y CERTIFICACIÓN (MESA DE AYUDA/DUPLICADOS/CERTIFICADOS EN EC Ó NTCL)</option>
-                  <option>DIRECCIÓN DE EXCELENCIA EN SERVICIO A USUARIOS (RENAC/QUEJAS)</option>
-                  <option>DIRECCIÓN GENERAL DE PROMOCIÓN Y DESARROLLO (CGC)</option>
+                  <option value="1">DIRECCIÓN DE PROMOCÍON Y APOYO A PRESTADORES (ACREDITACIÓN INICIAL)</option>
+                  <option value="2">DIRECCIÓN DE ACREDITACIÓN Y CERTIFICACIÓN (MESA DE AYUDA/DUPLICADOS/CERTIFICADOS EN EC Ó NTCL)</option>
+                  <option value="3">DIRECCIÓN DE EXCELENCIA EN SERVICIO A USUARIOS (RENAC/QUEJAS)</option>
+                  <option value="4">DIRECCIÓN GENERAL DE PROMOCIÓN Y DESARROLLO (CGC)</option>
                 </select>
               </div>
             </div>
             <div class="row panel-margin">
               <div class="col-md-6">
                 <label>Comentarios</label>
-                <div class="input-group">
-                  <span class="input-group-addon"><input id="Recordatorio" name="Recordatorio" type="checkbox" value="option1"></span>
-                  <textarea id="Comentarios" class="form-control" rows="4" placeholder="Comentarios"></textarea>
-                </div>
+                <textarea id="txtAreaComentarios" name="txtAreaComentarios" class="form-control" rows="4" placeholder="Comentarios"></textarea>
               </div>
             </div>
             <div class="row panel-margin">
@@ -61,29 +58,29 @@
                 <label>Estatus</label>
                 <div class="radio">
                   <label>
-                    <input type="radio" name="Resolucion" id="optionsRadios1" value="Atendido" checked>Atendido
+                    <input type="radio" name="rdioResolucion" id="rdioResolucion3" value="3" checked>Atendido
                   </label>
                 </div>
                 <div class="radio">
                   <label>
-                    <input type="radio" name="Resolucion" id="optionsRadios2" value="Tramite">En Trámite
+                    <input type="radio" name="rdioResolucion" id="rdioResolucion2" value="2">En Trámite
                   </label>
                 </div>
                 <div class="radio">
                   <label>
-                    <input type="radio" name="Resolucion" id="optionsRadios2" value="Pendiente">Pendiente
+                    <input type="radio" name="rdioResolucion" id="rdioResolucion1" value="1">Pendiente
                   </label>
                 </div>
               </div>
             </div>
             <div class="row">
               <div class="col-md-2">
-                <button id="btnAgregarVisita" class="btn btn-primary btn-lg">Agregar visita</button>
+                <button type="submit" id="btnAgregarVisita" class="btn btn-success">Agregar visita</button>
               </div>
               <div class="col-md-offset-1 col-md-2">
-                <button id="" class="btn btn-primary" style="display: none;">Nueva Visita</button>
+                <button id="" class="btn btn-primary">Nueva Visita</button>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
